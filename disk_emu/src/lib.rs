afef7f733b18d2aa4f30b43177a4820243037ba8
vladglv <vladglv@users.noreply.github.com>
2018-12-06 10:20:49 -0500

    Added the rust implementation of the disk_emu

--- disk_emu/src/lib.rs
+++ disk_emu/src/lib.rs
@@ -1,4 +1,179 @@
+#![no_std]
+extern crate libc;
+
+use libc::{c_char, c_void, FILE};
+
 #[no_mangle]
-pub extern "C" fn call_from_c() {
-    println!("Just called a Rust function from C!");
+pub unsafe extern "C" fn vl_close_disk(file: *mut FILE) -> i64 {
+    let mut r = -1;
+
+    if !file.is_null() {
+        r = libc::fclose(file);
+        assert!(r == 0);
+    }
+
+    i64::from(r)
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn vl_init_fresh_disk(
+    file: *mut *mut FILE,
+    filename: *const c_char,
+    size: i64,
+    num: i64,
+    block_size: *mut i64,
+    block_num: *mut i64,
+) -> i64 {
+    if size <= 0
+        || num <= 0
+        || filename.is_null()
+        || block_size.is_null()
+        || block_num.is_null()
+        || file.is_null()
+    {
+        return -1;
+    }
+
+    *block_size = size;
+    *block_num = num;
+
+    *file = libc::fopen(filename, "w+be".as_ptr() as *const _);
+    if (*file).is_null() {
+        return -1;
+    }
+
+    let buf = libc::calloc(size as usize, 1);
+    if buf.is_null() {
+        return -1;
+    }
+
+    for _i in 0..num {
+        assert!(libc::fwrite(buf, size as usize, 1, *file) == 1);
+    }
+
+    libc::free(buf);
+
+    0
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn vl_init_disk(
+    file: *mut *mut FILE,
+    filename: *const c_char,
+    size: i64,
+    num: i64,
+    block_size: *mut i64,
+    block_num: *mut i64,
+) -> i64 {
+    if size <= 0
+        || num <= 0
+        || filename.is_null()
+        || block_size.is_null()
+        || block_num.is_null()
+        || file.is_null()
+    {
+        return -1;
+    }
+
+    *block_size = size;
+    *block_num = num;
+
+    *file = libc::fopen(filename, "r+be".as_ptr() as *const _);
+    if (*file).is_null() {
+        return -1;
+    }
+
+    0
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn vl_write_blocks(
+    file: *mut FILE,
+    begin: i64,
+    num: i64,
+    block_size: i64,
+    block_num: i64,
+    buf: *const c_void,
+) -> i64 {
+    if file.is_null() || begin < 0 || num <= 0 || buf.is_null() {
+        return -1;
+    }
+
+    assert!(block_size > 0);
+    assert!(block_num > 0);
+
+    if begin + num > block_num {
+        return -1;
+    }
+
+    let offset = begin * block_size;
+    if libc::fseek(file, offset, libc::SEEK_SET) != 0 {
+        return -1;
+    }
+
+    let block_buf = libc::malloc(block_size as usize);
+    if block_buf.is_null() {
+        return -1;
+    }
+
+    let mut s = 0;
+    for i in 0..num {
+        let ptr = buf as *const u8;
+        let addr = ptr.offset((i * block_size) as isize);
+        libc::memcpy(block_buf, addr as *const _, block_size as usize);
+        assert!(libc::fwrite(block_buf, block_size as usize, 1, file) == 1);
+        assert!(libc::fflush(file) == 0);
+
+        s += 1;
+    }
+
+    libc::free(block_buf);
+
+    s
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn vl_read_blocks(
+    file: *mut FILE,
+    begin: i64,
+    num: i64,
+    block_size: i64,
+    block_num: i64,
+    buf: *mut c_void,
+) -> i64 {
+    if file.is_null() || begin < 0 || num <= 0 || buf.is_null() {
+        return -1;
+    }
+
+    assert!(block_size > 0);
+    assert!(block_num > 0);
+
+    if begin + num > block_num {
+        return -1;
+    }
+
+    let offset = begin * block_size;
+    if libc::fseek(file, offset, libc::SEEK_SET) != 0 {
+        return -1;
+    }
+
+    let block_buf = libc::malloc(block_size as usize);
+    if block_buf.is_null() {
+        return -1;
+    }
+
+    let mut s = 0;
+    for i in 0..num {
+        assert!(libc::fread(block_buf, block_size as usize, 1, file) == 1);
+
+        let ptr = buf as *mut u8;
+        let addr = ptr.offset((i * block_size) as isize);
+        libc::memcpy(addr as *mut _, block_buf, block_size as usize);
+
+        s += 1;
+    }
+
+    libc::free(block_buf);
+
+    s
 }
--- src/disk_emu.c
+++ src/disk_emu.c
@@ -1,163 +1,32 @@
 #include "disk_emu.h"
-#include <assert.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <time.h>
-#include <unistd.h>
 
-static FILE *fp = NULL;
+extern i64 vl_close_disk(FILE*);
+extern i64 vl_init_fresh_disk(FILE**, char*, i64, i64, i64*, i64*);
+extern i64 vl_init_disk(FILE**, char*, i64, i64, i64*, i64*);
+extern i64 vl_write_blocks(FILE*, i64, i64, i64, i64, const void*);
+extern i64 vl_read_blocks(FILE*, i64, i64, i64, i64, void*);
+
+static FILE* fp = NULL;
 static i64 BLOCK_SIZE = 0;
 static i64 MAX_BLOCK = 0;
 
-/*----------------------------------------------------------*/
-/*Close the disk file filled when you don't need it anymore. */
-/*----------------------------------------------------------*/
 i64 close_disk(void) {
-  int r = -1;
-
-  if (fp != NULL) {
-    r = fclose(fp);
-    assert(r == 0);
-  }
-
-  return r;
+  return vl_close_disk(fp);
 }
 
-/*---------------------------------------*/
-/*Initializes a disk file filled with 0's*/
-/*---------------------------------------*/
 i64 init_fresh_disk(char *filename, i64 block_size, i64 num_blocks) {
-  if (block_size <= 0 || num_blocks <= 0 || filename == NULL) {
-    return -1;
-  }
-
-  BLOCK_SIZE = block_size;
-  MAX_BLOCK = num_blocks;
-
-  /*Creates a new file*/
-  fp = fopen(filename, "w+be");
-  if (fp == NULL) {
-    printf("Could not create new disk file %s\n\n", filename);
-    return -1;
-  }
-
-  void *buf = calloc((size_t)block_size, 1);
-  if (buf == NULL) {
-    return -1;
-  }
-
-  /*Fills the file with 0's to its given size*/
-  for (i64 i = 0; i < num_blocks; i++) {
-    assert(fwrite(buf, (size_t)block_size, 1, fp) == 1);
-  }
-
-  free(buf);
-
-  return 0;
+  return vl_init_fresh_disk(&fp, filename, block_size, num_blocks, &BLOCK_SIZE, &MAX_BLOCK);
 }
-/*----------------------------*/
-/*Initializes an existing disk*/
-/*----------------------------*/
+
 i64 init_disk(char *filename, i64 block_size, i64 num_blocks) {
-  if (block_size <= 0 || num_blocks <= 0 || filename == NULL) {
-    return -1;
-  }
-
-  BLOCK_SIZE = block_size;
-  MAX_BLOCK = num_blocks;
-
-  /*Opens a file*/
-  fp = fopen(filename, "r+be");
-  if (fp == NULL) {
-    printf("Could not open %s\n\n", filename);
-    return -1;
-  }
-
-  return 0;
+  return vl_init_disk(&fp, filename, block_size, num_blocks, &BLOCK_SIZE, &MAX_BLOCK);
 }
 
-/*-------------------------------------------------------------------*/
-/*Reads a series of blocks from the disk into the buffer             */
-/*-------------------------------------------------------------------*/
 i64 read_blocks(i64 start_address, i64 nblocks, void *buffer) {
-  if (start_address < 0 || nblocks <= 0 || buffer == NULL) {
-    return -1;
-  }
-
-  i64 s = 0;
-
-  assert(BLOCK_SIZE > 0);
-  assert(MAX_BLOCK > 0);
-
-  /*Checks that the data requested is within the range of addresses of the
-   * disk*/
-  if (start_address + nblocks > MAX_BLOCK) {
-    printf("out of bound error %ld\n", start_address);
-    return -1;
-  }
-
-  /*Goto the data requested from the disk*/
-  i64 offset = start_address * BLOCK_SIZE;
-  if (fseek(fp, offset, SEEK_SET) != 0) {
-    return -1;
-  }
-
-  /*For every block requested*/
-  /*Sets up a temporary buffer*/
-  void *blockRead = malloc((size_t)BLOCK_SIZE);
-  if (blockRead == NULL) {
-    return -1;
-  }
-
-  for (i64 i = 0; i < nblocks; ++i) {
-    s++;
-    assert(fread(blockRead, (size_t)BLOCK_SIZE, 1, fp) == 1);
-    memcpy(((char *)buffer) + (i * BLOCK_SIZE), blockRead, (size_t)BLOCK_SIZE);
-  }
-
-  free(blockRead);
-  return s;
+  return vl_read_blocks(fp, start_address, nblocks, BLOCK_SIZE, MAX_BLOCK, buffer);
 }
 
-/*------------------------------------------------------------------*/
-/*Writes a series of blocks to the disk from the buffer             */
-/*------------------------------------------------------------------*/
 i64 write_blocks(i64 start_address, i64 nblocks, const void *buffer) {
-  if (start_address < 0 || nblocks <= 0 || buffer == NULL) {
-    return -1;
-  }
-
-  i64 s = 0;
-
-  assert(BLOCK_SIZE > 0);
-  assert(MAX_BLOCK > 0);
-
-  /*Checks that the data requested is within the range of addresses of the
-   * disk*/
-  if (start_address + nblocks > MAX_BLOCK) {
-    printf("out of bound error\n");
-    return -1;
-  }
-
-  /*Goto where the data is to be written on the disk*/
-  i64 offset = start_address * BLOCK_SIZE;
-  if (fseek(fp, offset, SEEK_SET) != 0) {
-    return -1;
-  }
-
-  void *blockWrite = malloc((size_t)BLOCK_SIZE);
-  /*For every block requested*/
-  for (i64 i = 0; i < nblocks; ++i) {
-    memcpy(blockWrite, ((const char *)buffer) + (i * BLOCK_SIZE),
-           (size_t)BLOCK_SIZE);
-
-    assert(fwrite(blockWrite, (size_t)BLOCK_SIZE, 1, fp) == 1);
-    assert(fflush(fp) == 0);
-    s++;
-  }
-
-  free(blockWrite);
-
-  return s;
+  return vl_write_blocks(fp, start_address, nblocks, BLOCK_SIZE, MAX_BLOCK, buffer);
 }
